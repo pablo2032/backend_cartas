0bae0c42fb01a876573e457927a5e2b0dbd29f1f
Pablo Miranda <pablo.mirandac@gmail.com>
2024-01-10 11:04:58 -0300

    build

--- dist/app.js
+++ dist/app.js
@@ -8,11 +8,12 @@ const morgan_1 = __importDefault(require("morgan"));
 const path_1 = __importDefault(require("path"));
 const cors_1 = __importDefault(require("cors"));
 const connect_busboy_1 = __importDefault(require("connect-busboy"));
+require('dotenv').config();
 const index_1 = __importDefault(require("./routes/index"));
 // Initializations
 const app = (0, express_1.default)();
 // Settings
-app.set('port', process.env.PORT || 4000);
+app.set('port', process.env.PORT || 3000);
 // Middlewares
 app.use((0, morgan_1.default)('dev'));
 app.use((0, cors_1.default)());
--- dist/database.js
+++ dist/database.js
@@ -2,8 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.startConnection = void 0;
 const mongoose_1 = require("mongoose");
+require('dotenv').config();
+const dbHost = process.env.MONGODB_URI;
 async function startConnection() {
-    const db = await (0, mongoose_1.connect)('mongodb+srv://pablo:Walala123@cluster0.up2i9ij.mongodb.net/cartas', {
+    const db = await (0, mongoose_1.connect)(process.env.MONGODB_URI, {
     //useNewUrlParser: true        
     });
     console.log('Database is connected');

19ae08e98aec68a9b758f7dc5503a8b54e7e5f79
Pablo Miranda <pablo.mirandac@gmail.com>
2024-01-12 15:02:27 -0300

    valida respuesta id

--- src/controllers/photo.controller.ts
+++ src/controllers/photo.controller.ts
@@ -39,11 +39,26 @@ export async function createPhoto(req: Request, res: Response): Promise<Response
 };
 
 export async function getPhoto(req: Request, res: Response): Promise<Response> {
-   
-    //usando destructuracion o const id = req.params.id;
-    const { id } = req.params;
-    const photo = await Photo.findById(id);
-    return res.json(photo);
+    try {
+        const { id } = req.params;
+
+        // Verificar si el ID proporcionado es válido antes de realizar la consulta
+        if (!isValidObjectId(id)) {
+            return res.status(400).json({ message: 'ID de foto no válido' });
+        }
+
+        const photo = await Photo.findById(id);
+
+        // Verificar si la foto existe
+        if (!photo) {
+            return res.status(404).json({ message: 'Foto no encontrada' });
+        }
+
+        return res.json(photo);
+    } catch (error) {
+        console.error('Error al obtener la foto:', error);
+        return res.status(500).json({ message: 'Error interno del servidor' });
+    }
 }
 
 export async function deletePhoto(req: Request, res: Response): Promise<Response> {// cuando se devuelve una promesa, se debe retornar un valor y con await
@@ -77,4 +92,9 @@ export async function updatePhoto(req: Request, res: Response): Promise<Response
         message: 'Successfully updated',
         updatedPhoto
     });
+}
+
+function isValidObjectId(id: string): boolean {
+    const ObjectId = require('mongoose').Types.ObjectId;
+    return ObjectId.isValid(id);
 }
